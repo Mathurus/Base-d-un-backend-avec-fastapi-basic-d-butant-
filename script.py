a27e2a8dcf22700657ff3e271116134575ea163e
Mathurus <127137206+Mathurus@users.noreply.github.com>
2026-02-09 12:36:30 +0100

    Refactor script.py for improved structure and readability

--- script.py
+++ script.py
@@ -7,18 +7,18 @@ app = FastAPI()
 
 @app.get("/configuration")   #toute le code qui suivrat sera éxécuter que si il y a une requete vers /configuration 
 def root():
-    with open("package.json", "rt") as f:   #ouvre le fichier package.json avec les droits de lecture 
-        c = json.load(f)       #charge au format json les informations 
-        return c    #retourne la variable c 
+    with open("package.json", "rt") as f:   
+        c = json.load(f)    
+        return c   
 
 local_users = [
     {"name": "user1",
-     "password": "quelquechose"}, #Dictionnaire des username et des password qui simule une base de donnée  
+     "password": "quelquechose"},
     {"name": "user2",
      "password": "autrechose"}
 ]
 
-key = "dezdezzedzehdeoizdhzeoihzi"  #clé qui nous servirat pour un token plus tard 
+key = "dezdezzedzehdeoizdhzeoihzi" 
 
 def chiffre(name, key):
     return base64.b64encode(json.dumps({"name": name, "date_expiration": "2025"}).encode()) #génération du token 
@@ -29,16 +29,16 @@ def verifie_token(b64token, key):
     return "name" in c
 
 
-@app.get("/login")     #action pour le /login 
+@app.get("/login")   
 def login(name:str, password:str):
-    print("name", name, password)  #affiche le user et son mot de passe 
+    print("name", name, password) 
     for user in local_users:
         if name == user['name']:    #vérifie si le user éxiste 
             print("Utilisateur trouvé") #si il éxiste il renvoie "Utilisateur trouvé "
             if password == user['password']:    #vérifie que le password correspond par rapport au user 
-                return chiffre(name, "dezdezzedzehdeoizdhzeoihzi")  #si ça correspond il génère le token avec les informations correspondant  
+                return chiffre(name, "dezdezzedzehdeoizdhzeoihzi")  
             else:
-                raise HTTPException(status_code=403, detail="Mot de passe erroné")  #sinon il renvoie une erreur 
+                raise HTTPException(status_code=403, detail="Mot de passe erroné")  
     else:
         return 404, "Non trouvé"
 
@@ -46,16 +46,15 @@ def login(name:str, password:str):
 def get_current_user():
     return "Mathurin"
 
-@app.get("/users")  #action pour le /users
+@app.get("/users")  
 def users(token):
-    # SQL-> BDD Utilisateurs
-    #
-    if verifie_token(token, key):   #si le user a un token valide ou spécifique (root) 
-        return [{"name": "user1"}, {"name": "user2"}]   #il renvoie le nom des user 
+    if verifie_token(token, key):  
+        return [{"name": "user1"}, {"name": "user2"}]  
     else:
-        return "Accès interdit" #sinon il affiche accès interdit 
+        return "Accès interdit" 
 
 
 if __name__ == '__main__':
     import copy
+
     uvicorn.run(app)    #lancement de l'application     
